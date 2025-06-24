7b89f0920bc55b905805ac1b703cec5e5276cd04
Royal-Ryan <subashshah170@gmail.com>
2025-06-25 02:43:28 +0545

    set variable port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app=Flask(__name__)
 
@@ -25,4 +26,5 @@ def fetch_conversion_factor(source,target):
     return response
 
 if __name__== "__main__":
-    app.run(debug=True)
+    port= int(os.environ.get("PORT",5000)) # Use port env var if set, else use port 5000
+    app.run(host="0.0.0.0",port = port)
